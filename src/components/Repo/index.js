8a62a06c82eb983b4499e87d6e75f33a66a75135
João Henrique Xavier <jhcxavier@Joaos-MacBook-Pro.local>
2022-04-13 14:42:25 -0700

    Update in repo component and flux

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -2,13 +2,13 @@ import React, {useContext} from 'react';
 import { Context } from '../../store/appContext';
 import styles from "./index.module.scss"
 
-const Repos=({repository})=>{
+const Repos=({repository, index})=>{
     const {actions, store}=useContext(Context)
     const url = repository.issues_url.split("{")
     
     return(
        <div className={styles.repoWrapper} onClick={()=>actions.getIssues(url[0], store.token)}>
-          <h3>{repository.name}</h3> 
+          <h3>{index+1}. {repository.name}</h3> 
        </div>
     )
 }
